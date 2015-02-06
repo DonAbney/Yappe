d481f260e79eca4427d3611ad88bff8cffb13691
Don Abney <tdonaldabney@gmail.com>
2015-02-06 17:15:06 -0500

    Initial Commit

---
+++ Yappe/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Yappe
+//
+//  Created by Don Abney on 2/6/15.
+//  Copyright (c) 2015 Don Abney. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
